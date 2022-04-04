d8068fd33cc7a1c96d78bdef40ae784d038eab35
lhbdawn <67319113+lhbdawn@users.noreply.github.com>
2022-04-04 19:03:56 +0530

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://imgur.com/undefined.png?";
+            i.src = "https://imgur.com/pcxKO8C.png?";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px; transition: opacity 0.2s ease";
             console.log(i);
             var t;
