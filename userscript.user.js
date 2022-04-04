001e74faf9fab051ba62047870578cde9679dd83
lhbdawn <67319113+lhbdawn@users.noreply.github.com>
2022-04-04 18:06:59 +0530

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Kaguya Server Template
+// @name         snafu Server Template
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the canvas!
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://imgur.com/2be7Rpu.png?";
+            i.src = "https://imgur.com/undefined.png?";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px; transition: opacity 0.2s ease";
             console.log(i);
             var t;
